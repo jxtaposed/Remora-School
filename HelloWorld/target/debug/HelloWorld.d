00a22b03fcd313bc0610c6b9decb77f64c4a5e5f
nirav <nshah42@gmail.com>
2023-02-18 16:23:58 -0500

    Commiting new files for Remora SchooL Rust programs as of 02-15-23

---
+++ HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/nshah/Documents/Remora\ School/HelloWorld/target/debug/HelloWorld: /mnt/c/Users/nshah/Documents/Remora\ School/HelloWorld/src/main.rs
---
+++ HelloWorld/target/debug/deps/HelloWorld-980279c1e2fd1c77.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/nshah/Documents/Remora School/HelloWorld/target/debug/deps/HelloWorld-980279c1e2fd1c77: src/main.rs
+
+/mnt/c/Users/nshah/Documents/Remora School/HelloWorld/target/debug/deps/HelloWorld-980279c1e2fd1c77.d: src/main.rs
+
+src/main.rs:
