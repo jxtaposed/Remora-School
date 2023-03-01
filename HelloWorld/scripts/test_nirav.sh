f7e8a12c77a2dc87d72ea533bb96e5c2346cca8d
nirav <nshah42@gmail.com>
2023-03-01 18:54:28 -0500

    Updated script to improve relative path

--- HelloWorld/scripts/test_nirav.sh
+++ HelloWorld/scripts/test_nirav.sh
@@ -2,13 +2,13 @@
 # Expectations
 expectedResult='Hello, world!'
 # Build
-cd /mnt/c/Users/Documents/Remora\ School/HelloWorld
+cd ../HelloWorld
 cargo build
 # Run
-actualResult=$(exec /mnt/c/Users/nshah/Documents/Remora\ School/HelloWorld/target/debug/HelloWorld)
+actualResult=$(exec ../HelloWorld/target/debug/HelloWorld)
 # Verify
 if [ "$expectedResult" = "$actualResult" ]; then 
-	echo "I done did it"
+	echo "I done did it, returned expected result:"
 else 
 	echo "$actualResult"
 fi
