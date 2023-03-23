a96ee78e5a7adc22b9c1649a1879dd334199bccc
nirav <nshah42@gmail.com>
2023-03-23 18:15:09 -0400

    Reading in Data: - Established pseudo code for the segments of the intended program - Built the csv read in lines given test file and additional query argument

--- csv_loader/src/main.rs
+++ csv_loader/src/main.rs
@@ -1,3 +1,25 @@
+//use std::io
+//use std::env
+// Establish functions that will read in csv data
+
+// Establish functions that will sort csv data
+
+// Establish functions that will print top N values
+use std::env;
+use std::fs;
+//fn main() {
+    // Main function should read in the csv data and convert to mut variable
+    
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let n = &args[2];
+    let file_path = &args[1];
+
+    println!("{}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("csv content:\n{contents}");
 }
