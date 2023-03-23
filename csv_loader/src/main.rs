1e84af6c9350bd3f3b0eef7acb3d573bac809168
nirav <nshah42@gmail.com>
2023-03-23 18:25:20 -0400

    Comments and functions cleanup: - Cleaned up some misplaced comments and some pseudocode - restructured the csv read in aspect of main to a function 'reader' which is called by main.

--- csv_loader/src/main.rs
+++ csv_loader/src/main.rs
@@ -1,16 +1,8 @@
-//use std::io
-//use std::env
-// Establish functions that will read in csv data
-
-// Establish functions that will sort csv data
-
-// Establish functions that will print top N values
 use std::env;
 use std::fs;
-//fn main() {
-    // Main function should read in the csv data and convert to mut variable
-    
-fn main() {
+
+// Establish functions that will read in csv data
+fn reader() {
     let args: Vec<String> = env::args().collect();
 
     let n = &args[2];
@@ -22,4 +14,12 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("csv content:\n{contents}");
+}
+// Establish functions that will sort csv data
+
+// Establish functions that will print top N values
+
+  
+fn main() {
+    reader()
 }
